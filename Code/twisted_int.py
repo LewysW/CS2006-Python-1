82834eb24c69753d4018b4037b4159a94a30e75b
LewysW <lewysward99@gmail.com>
2018-02-28 16:13:20 +0000

    Modified multiply and add methods to return the modulo of the result.

--- Code/twisted_int.py
+++ Code/twisted_int.py
@@ -1,15 +1,17 @@
 class TwistedInt:
 
-    def __init__(self, obj):
+    def __init__(self, obj, mod):
         self.object = obj
+        self.mod = mod
 
     # overwrite "print"
     def __str__(self):
-        return "<"+str(self.object)+">"
+        return "<" + str(self.object)+ ":" + str(self.mod) + ">"
 
     # define "*"
     def __mul__(self, other):
-        return TwistedInt(self.object + other.object + self.object * other.object)
+        return TwistedInt((self.object + other.object + self.object * other.object) % self.mod, self.mod)
 
-    def hello(self):
-        return "Hello, World!"
+    # define "+"
+    def __add__(self, other):
+        return TwistedInt((self.object + other.object) % self.mod, self.mod)
