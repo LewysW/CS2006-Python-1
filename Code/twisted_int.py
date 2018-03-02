466e9daf867c7fa5d1a0e2b3ff20692392e83532
locw <locw@pc3-062-l.cs.st-andrews.ac.uk>
2018-03-02 16:48:51 +0000

    Completed functions for proving commutative properties for easy requirement 2

--- Code/twisted_int.py
+++ Code/twisted_int.py
@@ -1,3 +1,5 @@
+import itertools
+
 class TwistedInt:
 
     # TODO Add unit tests for current functions
@@ -64,29 +66,74 @@ class TwistedInt:
 
     #Easy Requirement 2.1
     @staticmethod
-    def commutativeAdd(n):
-        for x in range(0, n):
-            for y in range(0, n):
-                a = TwistedInt(x, n)
-                b = TwistedInt(y, n)
-                print(a)
-                print(b)
-                if (a + b).object != (b + a).object:
-                    return False
+    def isCommutativeAdd(n):
+        xlist = ylist = [x for x in range(0, n)]
+
+        for x, y in itertools.product(xlist, ylist):
+            a = TwistedInt(x, n)
+            b = TwistedInt(y, n)
+
+            if (a + b).object != (b + a).object:
+                return False
 
         return True
 
     #Easy Requirement 2.2
     @staticmethod
-    def commutativeMul(n):
-        for x in range(0, n):
-            for y in range(0, n):
-                a = TwistedInt(x, n)
-                b = TwistedInt(y, n)
-                print(a)
-                print(b)
-                if (a * b).object != (b * a).object:
-                    return False
+    def isCommutativeMul(n):
+        xlist = ylist = [x for x in range(0, n)]
+
+        for x, y in itertools.product(xlist, ylist):
+            a = TwistedInt(x, n)
+            b = TwistedInt(y, n)
+
+            if (a * b).object != (b * a).object:
+                return False
+
+        return True
+
+    #Easy Requirement 2.3
+    @staticmethod
+    def isCommutativePrecedenceAdd(n):
+        xlist = ylist = zlist = [x for x in range(0, n)]
+
+        for x, y, z in itertools.product(xlist, ylist, zlist):
+            a = TwistedInt(x, n)
+            b = TwistedInt(y, n)
+            c = TwistedInt(z, n)
+
+            if ((a + b) + c).object != (a + (b + c)).object:
+                return False
+
+        return True
+
+    #Easy Requirement 2.4
+    @staticmethod
+    def isCommutativePrecedenceMul(n):
+        xlist = ylist = zlist = [x for x in range(0, n)]
+
+        for x, y, z in itertools.product(xlist, ylist, zlist):
+            a = TwistedInt(x, n)
+            b = TwistedInt(y, n)
+            c = TwistedInt(z, n)
+
+            if ((a * b) * c).object != (a * (b * c)).object:
+                return False
+
+        return True
+
+    #Easy Requirement 2.5
+    @staticmethod
+    def isCommutativeCommonFac(n):
+        xlist = ylist = zlist = [x for x in range(0, n)]
+
+        for x, y, z in itertools.product(xlist, ylist, zlist):
+            a = TwistedInt(x, n)
+            b = TwistedInt(y, n)
+            c = TwistedInt(z, n)
+
+            if ((a + b) * c).object != ((a * c) + (b * c)).object:
+                return False
 
         return True
 
