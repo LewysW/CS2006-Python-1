d7c52c667e5fa59a2892ad1e04c87b8e58e03c01
locw <locw@pc3-012-l.cs.st-andrews.ac.uk>
2018-03-03 15:11:02 +0000

    Integrated IteratorOfTwistedIntegers into same file as TwistedIntegers

--- Code/main.py
+++ Code/main.py
@@ -1,3 +1,2 @@
 from checker import *
-from twisted_integer import *
-from iterator_of_twisted_integers import *
+from twisted_integers import *
--- Code/twisted_integers.py
+++ Code/twisted_integers.py
@@ -14,9 +14,31 @@ class TwistedIntegers:
             self.n = n
             for i in range(n):
                 self.integers.append(TwistedInt(i, n))
+            self.iterator = IteratorOfTwistedIntegers(self)
 
+        # Define iterator instance here
         def __str__(self):
-            return "<_:" + str(self.n) + ">"
+            out = ""
+
+            while self.iterator.hasNext():
+                out += str(self.iterator.next()) + "\n"
+            return out
 
         def Size(self):
             return self.n - 1
+
+class IteratorOfTwistedIntegers:
+    def __init__(self, twistedIntegers):
+        self.twistedIntegers = twistedIntegers
+        self.index = 0
+
+    def hasNext(self):
+        return self.index <= self.twistedIntegers.Size()
+
+    def next(self):
+        if not(self.hasNext()):
+            raise IndexError("Index of iterator is greater than or equal to length of twistedIntegers")
+
+        element = self.twistedIntegers.integers[self.index]
+        self.index += 1
+        return element
