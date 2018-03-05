a68e3ccf322a0d90a25cc42f4a7d866475084282
Gavin Sandford <gdms@st-andrews.ac.uk>
2018-03-05 17:46:54 +0000

    comments and optimisation

--- Code/checker.py
+++ Code/checker.py
@@ -1,24 +1,36 @@
-"""Checks whether or not for a given n the following properties hold for all x, y, z ∈ Zn:"""
-
 from twisted_int import *
+from itertools import combinations_with_replacement
+from itertools import product
 
 #TODO throw exceptions for negatives
 def isCommutativeAdd(n):
     """Tests that x ⊕ y = y ⊕ x for all x, y, z ∈ Zn
 
     Tests for every value under n, ignoring duplicates.
-    e.g. Once <0:n> + <1:n>  = <1:n> + <0:n> has been confirmed,
+    Once <0:n> + <1:n>  = <1:n> + <0:n> has been confirmed,
     no need to test <1:n> + <0:n> = <0:n> + <1:n>
-    """
-    numList = [x for x in range(n)]
-    numStr = ''.join(str(y) for y in numList)
 
-    for nums in itertools.combinations_with_replacement(numStr,2):
-        x = int(nums[0])
-        y = int(nums[1])
-        a = TwistedInt(x, n)
-        b = TwistedInt(y, n)
-        print(str(a) + " + " + str(b))
+    Args:
+        n: value for use in finding Zn. (tests for all values under n)
+
+    Returns:
+        bool: True if true for all values, False otherwise
+
+    Examples:
+        >>> isCommutativeAdd(2)
+        <0:2> <0:2>
+        <0:2> <1:2>
+        <1:2> <1:2>
+        True
+    """
+
+    numList = range(n)
+    numStr = ''.join(str(e) for e in numList)
+
+    for nums in combinations_with_replacement(numStr, 2):
+        x = int(nums[0]); a = TwistedInt(x, n)
+        y = int(nums[1]); b = TwistedInt(y, n)
+        print(str(a) + " " + str(b))
 
         if (a + b).object != (b + a).object:
             return False
@@ -29,18 +41,30 @@ def isCommutativeMul(n):
     """Tests x ⊗ y = y ⊗ x for all x, y, z ∈ Zn
     
     Tests for every value under n, ignoring duplicates.
-    e.g. Once <0:n> * <1:n>  = <1:n> * <0:n> has been confirmed,
-    no need to test <1:n> * <0:n> = <0:n> * <1:n>"""
+    Once <0:n> * <1:n>  = <1:n> * <0:n> has been confirmed,
+    no need to test <1:n> * <0:n> = <0:n> * <1:n>
 
-    numList = [x for x in range(n)]
-    numStr = ''.join(str(y) for y in numList)
+    Args:
+        n: value for use in finding Zn. (tests for all values under n)
 
-    for nums in itertools.combinations_with_replacement(numStr,2):
-        x = int(nums[0])
-        y = int(nums[1])
-        a = TwistedInt(x, n)
-        b = TwistedInt(y, n)
-        print(str(a) + " * " + str(b))
+    Returns:
+        bool: True if true for all values, False otherwise
+
+    Examples:
+        >>> isCommutativeMul(2)
+        <0:2> <0:2>
+        <0:2> <1:2>
+        <1:2> <1:2>
+        True
+    """
+
+    numList = range(n)
+    numStr = ''.join(str(e) for e in numList)
+
+    for nums in combinations_with_replacement(numStr, 2):
+        x = int(nums[0]); a = TwistedInt(x, n)
+        y = int(nums[1]); b = TwistedInt(y, n)
+        print(str(a) + " " + str(b))
 
         if (a * b).object != (b * a).object:
             return False
@@ -48,19 +72,35 @@ def isCommutativeMul(n):
     return True
 
 def isCommutativePrecedenceAdd(n):
-    """Tests (x ⊕ y) ⊕ z = x ⊕ (y ⊕ z)"""
+    """Tests (x ⊕ y) ⊕ z = x ⊕ (y ⊕ z)
+    
+    Tests for every value under n.
+    Ignoring duplicates is harder here so they are currently not ignored.
 
-    numList = [x for x in range(n)]
-    numStr = ''.join(str(y) for y in numList)
+    Args:
+        n: value for use in finding Zn. (tests for all values under n)
 
-    for nums in itertools.combinations_with_replacement(numStr,3):
-        x = int(nums[0])
-        y = int(nums[1])
-        z = int(nums[2])
-        a = TwistedInt(x, n)
-        b = TwistedInt(y, n)
-        c = TwistedInt(z, n)
-        print("(" + str(a) + " + " + str(b) + ") + " + str(c))
+    Returns:
+        bool: True if true for all values, False otherwise
+
+    Examples:
+        >>> isCommutativePrecedenceAdd(2)
+        <0:2> <0:2> <0:2>
+        <0:2> <0:2> <1:2>
+        <0:2> <1:2> <0:2>
+        <0:2> <1:2> <1:2>
+        <1:2> <0:2> <0:2>
+        <1:2> <0:2> <1:2>
+        <1:2> <1:2> <0:2>
+        <1:2> <1:2> <1:2>
+        True
+    """
+
+    for nums in getAllCombinations(n, 3):
+        x = int(nums[0]); a = TwistedInt(x, n)
+        y = int(nums[1]); b = TwistedInt(y, n)
+        z = int(nums[2]); c = TwistedInt(z, n)
+        print(str(a) + " " + str(b) + " " + str(c))
 
         if ((a + b) + c).object != (a + (b + c)).object:
             return False
@@ -68,19 +108,35 @@ def isCommutativePrecedenceAdd(n):
     return True
 
 def isCommutativePrecedenceMul(n):
-    """Tests (x ⊗ y) ⊗ z = x ⊗ (y ⊗ z)"""
+    """Tests (x ⊗ y) ⊗ z = x ⊗ (y ⊗ z)
 
-    numList = [x for x in range(n)]
-    numStr = ''.join(str(y) for y in numList)
+    Tests for every value under n.
+    Ignoring duplicates is not done to the same level here, as with 3 values, the possible combinations is much greater.
 
-    for nums in itertools.combinations_with_replacement(numStr,3):
-        x = int(nums[0])
-        y = int(nums[1])
-        z = int(nums[2])
-        a = TwistedInt(x, n)
-        b = TwistedInt(y, n)
-        c = TwistedInt(z, n)
-        print("(" + str(a) + " * " + str(b) + ") * " + str(c))
+    Args:
+        n: value for use in finding Zn. (tests for all values under n)
+
+    Returns:
+        bool: True if true for all values, False otherwise
+
+    Examples:
+        >>> isCommutativePrecedenceMul(2)
+        <0:2> <0:2> <0:2>
+        <0:2> <0:2> <1:2>
+        <0:2> <1:2> <0:2>
+        <0:2> <1:2> <1:2>
+        <1:2> <0:2> <0:2>
+        <1:2> <0:2> <1:2>
+        <1:2> <1:2> <0:2>
+        <1:2> <1:2> <1:2>
+        True
+    """
+
+    for nums in getAllCombinations(n, 3):
+        x = int(nums[0]); a = TwistedInt(x, n)
+        y = int(nums[1]); b = TwistedInt(y, n)
+        z = int(nums[2]); c = TwistedInt(z, n)
+        print(str(a) + " " + str(b) + " " + str(c))
 
         if ((a * b) * c).object != (a * (b * c)).object:
             return False
@@ -88,21 +144,58 @@ def isCommutativePrecedenceMul(n):
     return True
 
 def isCommutativeCommonFac(n):
-    """Tests (x ⊕ y) ⊗ z = (x ⊗ z) ⊕ (y ⊗ z)"""
+    """Tests (x ⊕ y) ⊗ z = (x ⊗ z) ⊕ (y ⊗ z)
 
-    numList = [x for x in range(n)]
-    numStr = ''.join(str(y) for y in numList)
+    Tests for every value under n.
+    Ignoring duplicates is harder here so they are currently not ignored.
 
-    for nums in itertools.combinations_with_replacement(numStr,3):
-        x = int(nums[0])
-        y = int(nums[1])
-        z = int(nums[2])
-        a = TwistedInt(x, n)
-        b = TwistedInt(y, n)
-        c = TwistedInt(z, n)
-        print("(" + str(a) + " + " + str(b) + ") * " + str(c))
+    Args:
+        n: value for use in finding Zn. (tests for all values under n)
+
+    Returns:
+        bool: True if true for all values, False otherwise
+
+    Examples:
+        >>> isCommutativeCommonFac(2)
+        <0:2> <0:2> <0:2>
+        <0:2> <0:2> <1:2>
+        <0:2> <1:2> <0:2>
+        <0:2> <1:2> <1:2>
+        <1:2> <0:2> <0:2>
+        <1:2> <0:2> <1:2>
+        <1:2> <1:2> <0:2>
+        <1:2> <1:2> <1:2>
+        True
+    """
+
+    for nums in getAllCombinations(n, 3):
+        x = int(nums[0]); a = TwistedInt(x, n)
+        y = int(nums[1]); b = TwistedInt(y, n)
+        z = int(nums[2]); c = TwistedInt(z, n)
+        print(str(a) + " " + str(b) + " " + str(c))
 
         if ((a + b) * c).object != ((a * c) + (b * c)).object:
             return False
 
     return True
+
+def getAllCombinations(n, r):
+    """Returns a sorted list of all the possible combinations of the numbers under a value.
+
+    Args:
+        n: the value to find combinations under
+        r: the number of combinations of values to find
+
+    Returns:
+        a set of all combinations
+
+    Examples:
+        >>> getAllCombinations(2,3)
+        [(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1), (1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1)]
+        
+        >>> getAllCombinations(3,2)
+        [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+
+    """
+    numList = set(product(range(n), repeat = r))
+    return sorted(numList)
